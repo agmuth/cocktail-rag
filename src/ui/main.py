02a1a8dd3cf2f81fc182e15e02b2affc7c15a665
agmuth <agmuth@gmail.com>
2024-07-24 16:51:35 -0600

    check point - ui not connecting to api

--- src/ui/main.py
+++ src/ui/main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import requests
-from src.settings import BASE_SETTINGS
+# from src.settings import BASE_SETTINGS
 
 question_input = st.text_input("Question:")
 
 if question_input:
-    response = requests.post(f"{BASE_SETTINGS.API_URL}/query/", json={"txt": question_input})
+    # url = f"{BASE_SETTINGS.API_URL}/query/"
+    url = f"http://fastapi:8000/query/"
+    response = requests.post(url, json={"txt": question_input})
     if response.status_code != 200:
         raise Exception(f"Error: API request unsuccessful. Status code: {response.status_code}")
     answer = response.json()["txt"]

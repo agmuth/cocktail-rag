ced10d5fb53f56819dc8b04d91e8cdf3c66a7a44
agmuth <agmuth@gmail.com>
2024-07-24 12:33:04 -0600

    app working sync

--- src/app/routers/query.py
+++ src/app/routers/query.py
@@ -6,13 +6,7 @@ router = APIRouter(
     prefix="/query",
 )
 
-# @router.post("/", response_model=QueryResponse)
-# async def query(query_request: QueryRequest):
-#     response = await query_engine.aquery(query_request.txt) 
-#     return QueryResponse(txt=response.response)
-
-
-@router.post("/", response_model=str)
-def query(query_request: str):
-    response = query_engine.query(query_request) 
-    return response.response
+@router.post("/", response_model=QueryResponse)
+def query(query_request: QueryRequest) -> QueryResponse:
+    response = query_engine.query(query_request.txt) 
+    return QueryResponse(txt=response.response)
